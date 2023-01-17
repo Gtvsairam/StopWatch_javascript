0c76842459e7eccf0e31eee3829c2929379a784d
Gtv Sairam <gtvsairam34@gmail.com>
2023-01-17 20:28:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+var hour = 00;
+var minute = 00;
+var second = 00;
+var count = 00;
+var timer = false;
+
+function start() {
+    timer = true
+    stopwatch();
+
+}
+
+function stop() {
+    timer = false
+}
+
+function reset() {
+    timer = false
+    hour = 0
+    minute = 0
+    second = 0
+    count = 0
+
+    document.getElementById('hr').innerHTML = "00";
+    document.getElementById('min').innerHTML = "00";
+    document.getElementById('Sec').innerHTML = "00";
+    document.getElementById('count').innerHTML = "00";
+}
+
+function stopwatch() {
+    if (timer) {
+        count++
+    }
+    if (count == 100) {
+        second++
+        count = 0
+    }
+    if (second == 60) {
+        minute++
+        second = 0
+    }
+    if (minute == 60) {
+        hour++
+        minute = 0
+        second = 0
+    }
+    let hrString = hour;
+    let minString = minute;
+    let secString = second;
+    let countString = count;
+
+    if (hour < 10) {
+        hrString = "0" + hrString
+    }
+    if (minute < 10) {
+        minString = "0" + minString
+    }
+    if (second < 10) {
+        secString = "0" + secString
+    }
+    if (count < 10) {
+        countString = "0" + countString
+    }
+
+    document.getElementById('hr').innerHTML = hrString;
+    document.getElementById('min').innerHTML = minString;
+    document.getElementById('sec').innerHTML = secString;
+    document.getElementById('count').innerHTML = countString;
+
+    setTimeout(stopwatch, 10);
+}
+
